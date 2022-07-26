605b33e582bf3afc411b7374671ad897751c08e0
oa_yhlee <yhlee@oa-world.com>
2022-07-26 19:04:35 +0900

    추가 파일

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'oaworld/onlinemall_collect',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'oaworld/onlinemall_collect' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
